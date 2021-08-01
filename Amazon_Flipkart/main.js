e4d09c68b7dcb7080c513487210f646f5692284e
Saurabh Sheshrao Khandagale <59130610+Khandagale-Saurabh@users.noreply.github.com>
2021-08-02 01:26:58 +0530

    Add files via upload

---
+++ Amazon_Flipkart/main.js
@@ -0,0 +1,25 @@
+
+//https://flaviocopes.com/puppeteer/
+let pup=require("puppeteer");
+let links=["https://www.awazon.com","https://www.flipkart.com"]
+let pname=process.args[2]
+(async function ()
+{
+        try {
+            let browser= await pup.launch({
+            headless: false,
+            defaultViewport: null,
+            args: ["--start-maximized",]
+        });
+            await getallinks(links[0],browser,pname);
+            );
+            
+        } catch (error) {
+            console.log(error);
+        }
+
+})();
+async function getallinks(links,browser,pname) 
+{
+    
+}
